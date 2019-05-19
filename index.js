b1957a00a7c94d2581200d113b0b347ab1570a6f
Gabriel Ferreiro <gbril9119@gmail.com>
2019-05-19 20:26:51 +0200

    Descomentados metodos de control de grupos

--- index.js
+++ index.js
@@ -92,19 +92,19 @@ app.route('/list-groups')
     res.sendStatus(200);
   });
 
-// app.route('/group-on')
-//     .get((req, res) => {
-//         const group = getGroupByName(config.group); // Sacar a configuracion
-//         tradfriClient.operateGroup(group, { onOff: true, dimmer: 100 });
-//         res.sendStatus(200);
-//     });
+app.route('/group-on')
+    .get((req, res) => {
+        const group = getGroupByName(config.group); // Sacar a configuracion
+        tradfriClient.operateGroup(group, { onOff: true, dimmer: 100 });
+        res.sendStatus(200);
+    });
 
-// app.route('/group-off')
-//     .get((req, res) => {
-//         const group = getGroupByName(config.group);
-//         tradfriClient.operateGroup(group, { onOff: false });
-//         res.sendStatus(200);
-//     });
+app.route('/group-off')
+    .get((req, res) => {
+        const group = getGroupByName(config.group);
+        tradfriClient.operateGroup(group, { onOff: false });
+        res.sendStatus(200);
+    });
 
 app.route('/finish')
   .get((req, res) => {
